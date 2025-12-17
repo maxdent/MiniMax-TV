dc5fb1df5aec5f1a5c8241f60e150b03d3067c0a
hxwyg <6g6bhxwyg>
2025-12-17 15:32:12 +0800

    修改

--- app/src/main/java/com/tvapp/minimax/MainActivity.kt
+++ app/src/main/java/com/tvapp/minimax/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.*
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : FragmentActivity() {
 
